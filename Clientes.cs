a88de2a89c55503d0455ac87a492c8410bd3ef4a
noemig1 <54558604+noemig1@users.noreply.github.com>
2019-08-26 18:53:12 -0400

    Add files via upload

---
+++ Clientes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Pventa
+{
+    public partial class Clientes : Form
+    {
+        public Clientes()
+        {
+            InitializeComponent();
+        }
+
+        private void clienteBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.clienteBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.proventasDataSet);
+
+        }
+
+        private void Clientes_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'proventasDataSet.Cliente' Puede moverla o quitarla según sea necesario.
+            this.clienteTableAdapter.Fill(this.proventasDataSet.Cliente);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ciTextBox.Clear();
+            nombreTextBox.Text = "";
+            apellidoTextBox.Text = "";
+            celularTextBox.Text = "";
+            direccionTextBox.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            clienteTableAdapter.Insert(ciTextBox.Text,nombreTextBox.Text,apellidoTextBox.Text,celularTextBox.Text,direccionTextBox.Text);
+            clienteTableAdapter.Fill(proventasDataSet.Cliente);
+        }
+    }
+}
